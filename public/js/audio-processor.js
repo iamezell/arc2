cca6d95d0c94dfd9d303ad2dec947c45d123791e
iamezell <iamezell@gmail.com>
2019-11-07 07:10:04 -0500

    finally

---
+++ public/js/audio-processor.js
@@ -0,0 +1,12 @@
+class AudioProcessor extends AudioWorkletProcessor {
+    process(inputs, outputs, parameters) {
+        const input = inputs[0];
+        const output = outputs[0];
+        for (let channel = 0; channel < output.length; ++channel) {
+            output[channel].set(input[channel]);
+          }
+        return true;
+    }
+}
+
+registerProcessor('audio-processor', AudioProcessor);
--- src/client/app.js
+++ src/client/app.js
@@ -1,5 +1,6 @@
 // import Player from './worldobjects/Player';
-import 'ws-audio-api'
+// import 'ws-audio-api'
+import workletUrl from '../../public/js/audio-processor';
 
 export default class App {
   constructor () {
@@ -20,12 +21,12 @@ export default class App {
 //         port: 80 //websockets server port
 // }});
 
-//   let streamerStartBtn = document.getElementById("streamerStartBtn")
+  let streamerStartBtn = document.getElementById("streamerStartBtn")
 //   let streamerStopBtn = document.getElementById("streamerStopBtn")
 
-//   streamerStartBtn.addEventListener("click", ()=>{
-//     streamer.start()
-//   }, this)
+  // streamerStartBtn.addEventListener("click", ()=>{
+  //   streamer.start()
+  // }, this)
 
 //   streamerStopBtn.addEventListener("click", ()=>{
 //     streamer.stop()
@@ -35,34 +36,68 @@ export default class App {
   //   let soundController = {}
   //   soundController.recording = false
 
-  let makeStream = (socket) => {
-    navigator.getUserMedia({ audio: true }, function(stream) {
-      let stream = stream;
-      let audioInput = audioContext.createMediaStreamSource(stream);
-      let gainNode = audioContext.createGain();
-      let recorder = audioContext.createScriptProcessor(_this.config.codec.bufferSize, 1, 1);
-      recorder.onaudioprocess = function(e) {
-        var resampled = _this.sampler.resampler(e.inputBuffer.getChannelData(0));
-        var packets = _this.encoder.encode_float(resampled);
-        for (var i = 0; i < packets.length; i++) {
-          if (socket.readyState == 1) socket.send(packets[i]);
-        }
-      };
-      audioInput.connect(_this.gainNode);
-      gainNode.connect(_this.recorder);
-      recorder.connect(audioContext.destination);
-    }, (err)=>{console.log("error", err)});
-  }
-    let exampleSocket = new WebSocket("wss://localhost", "protocolOne")
+  // let makeStream = (socket) => {
+  //   navigator.getUserMedia({ audio: true }, function(stream) {
+  //     let stream = stream;
+  //     let audioInput = audioContext.createMediaStreamSource(stream);
+  //     let gainNode = audioContext.createGain();
+  //     let recorder = audioContext.createScriptProcessor(_this.config.codec.bufferSize, 1, 1);
+  //     recorder.onaudioprocess = function(e) {
+  //       var resampled = _this.sampler.resampler(e.inputBuffer.getChannelData(0));
+  //       var packets = _this.encoder.encode_float(resampled);
+  //       for (var i = 0; i < packets.length; i++) {
+  //         if (socket.readyState == 1) socket.send(packets[i]);
+  //       }
+  //     };
+  //     audioInput.connect(gainNode);
+  //     gainNode.connect(recorder);
+  //     recorder.connect(audioContext.destination);
+  //   }, (err)=>{console.log("error", err)});
+  // }
+  //   let exampleSocket = new WebSocket("wss://localhost", "protocolOne")
 
-  //   // audioWorkletContext.connect(context.destination)
-  //   // for legacy browsers
-    exampleSocket.onopen = function (event) {
-      // exampleSocket.send("Here's some text that the server is urgently awaiting!");
-      makeStream( exampleSocket)
-      exampleSocket.close() 
-    };
-  //   const AudioContext = window.AudioContext || window.webkitAudioContext
+  // //   // audioWorkletContext.connect(context.destination)
+  // //   // for legacy browsers
+  //   exampleSocket.onopen = function (event) {
+  //     // exampleSocket.send("Here's some text that the server is urgently awaiting!");
+  //     makeStream( exampleSocket)
+  //     exampleSocket.close() 
+  //   };
+
+      //  const AudioContext = new window.AudioContext() || window.webkitAudioContext
+
+      //  AudioContext.audioWorklet.addModule(workletUrl).then(() => {
+      //   navigator.mediaDevices.getUserMedia({ audio: true }, stream => {
+      //     let microphone = AudioContext.createMediaStreamSource(stream);
+      //     let oggEncoder = new AudioWorkletNode(context, 'ogg');
+          
+      //     microphone.connect(oggEncoder).connect(AudioContext.destination);
+      //   });
+      // });
+
+      // var person = prompt("Please enter your name", "Harry Potter");
+
+      // if (person != null) {
+      //   touchStarted()
+      // }
+      streamerStartBtn.addEventListener("click", ()=>{
+        touchStarted()
+      }, this)
+
+      
+      const touchStarted = async ()=> {
+        const AudioContext = new window.AudioContext() || window.webkitAudioContext
+
+        await AudioContext.audioWorklet.addModule(workletUrl)
+         navigator.mediaDevices.getUserMedia({ audio: true }, stream => {
+           let microphone = AudioContext.createMediaStreamSource(stream);
+           let oggEncoder = new AudioWorkletNode(context, 'ogg');
+           
+           microphone.connect(oggEncoder).connect(AudioContext.destination);
+         });
+       
+      }
+      
 
   //   // const audioContext = new AudioContext()
 
